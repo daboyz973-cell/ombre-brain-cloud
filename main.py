bbcbd91220dd3042a4403f7a3e8a14fe6571b751
daboyz973-cell <daboyz973@gmail.com>
2026-06-22 11:40:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-    from fastapi import FastAPI
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-return {"message": "brain cloud is running"}
+    return {"message": "brain cloud is running"}
